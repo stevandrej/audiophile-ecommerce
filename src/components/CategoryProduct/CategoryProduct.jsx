a9b0a85cbd1bdb884d368bec1a67d5f4c7db2d61
Andrej Stevkovski <stevandrej@yahoo.com>
2021-09-09 23:54:39 +0200

    Updated Product Item in Category View

--- src/components/CategoryProduct/CategoryProduct.jsx
+++ src/components/CategoryProduct/CategoryProduct.jsx
@@ -3,14 +3,18 @@ import Button from "../Buttons/Button";
 import mark2Image from "../../assets/images/category-headphones/desktop/image-xx99-mark-two.jpg";
 import styles from "./CategoryProduct.module.scss";
 
-const CategoryProduct = () => {
+const CategoryProduct = ({ newProduct = false, reverseDirection = false }) => {
     return (
-        <div className={styles["product-container"]}>
+        <div className={`${styles["product-container"]} ${reverseDirection && styles['reverse-direction']}`}>
             <div className={styles["product-image"]}>
                 <img src={mark2Image} alt="mark 2" />
             </div>
             <div className={styles["product-info"]}>
-                <span className={`${styles.badge} overline`}>NEW PRODUCT</span>
+                {newProduct && (
+                    <span className={`${styles.badge} overline`}>
+                        NEW PRODUCT
+                    </span>
+                )}
                 <h2 className={styles.title}>XX99 MARK II HEADPHONES</h2>
                 <p>
                     The new XX99 Mark II headphones is the pinnacle of pristine
