f0343f7d1843d4a92059644b92682d2108862452
Andrej Stevkovski <stevandrej@yahoo.com>
2021-10-01 12:22:02 +0200

    Update
    added onClick functionality

--- src/components/Buttons/LinkButton.jsx
+++ src/components/Buttons/LinkButton.jsx
@@ -3,9 +3,9 @@ import styles from './button.module.scss';
 import IconArrow from '../../assets/icons/icon-arrow-right.svg';
 import { Link } from 'react-router-dom';
 
-const LinkButton = ({ variant = 'primary', href, children }) => {
+const LinkButton = ({ variant = 'primary', href, click, children }) => {
 
-	return <Link className={`${styles.btn} ${styles[variant]}`} to={href}>
+	return <Link className={`${styles.btn} ${styles[variant]}`} to={href} onClick={click}>
 		{children}
 		{/* Arrow icon > for tertiary variant button */}
 		{variant === 'tertiary' && <img className={styles.arrow} src={IconArrow} alt='arrow' />}
